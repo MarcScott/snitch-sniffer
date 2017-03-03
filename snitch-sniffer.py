12c4c693e95438ceadcf3f4fb39c83ce1ade712f
Harry Potter <h.potter@hogwarts.prog>
2017-03-03 20:27:17 +0000

    delete find function`

--- snitch-sniffer.py
+++ snitch-sniffer.py
@@ -4,8 +4,6 @@ from gpiozero import MotionSensor
 ms1 = MotionSensor(4)
 ms2 = MotionSensor(17)
 
-def find_snitch():
-    if ms1.motion_detected or if ms2.motion_detected:
-        return True
+
 
     
